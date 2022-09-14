aae1f9cbe64552f878d6a056a0cb440ecd28f696
雾歌 <zhaoyq_3810@163.com>
2022-09-14 15:06:14 +0800

    no message

--- components.d.ts
+++ components.d.ts
@@ -7,8 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    MobileTabBar: typeof import('./src/components/TabBar/MobileTabBar.vue')['default']
-    NButton: typeof import('naive-ui')['NButton']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NGlobalStyle: typeof import('naive-ui')['NGlobalStyle']
     NIcon: typeof import('naive-ui')['NIcon']
@@ -16,8 +14,6 @@ declare module '@vue/runtime-core' {
     NPopover: typeof import('naive-ui')['NPopover']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TabBar: typeof import('./src/components/TabBar/WebTabBar.vue')['default']
-    WebIcon: typeof import('./src/components/TabBar/WebIcon.vue')['default']
-    WebTabBar: typeof import('./src/components/TabBar/WebTabBar.vue')['default']
+    TabBar: typeof import('./src/components/TabBar.vue')['default']
   }
 }
